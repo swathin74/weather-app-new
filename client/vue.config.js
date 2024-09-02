db662a483068192da00f7b4c7b28a9b2a72f7583
Swathi Nayudu <swatin74@gmail.com>
2024-09-02 16:44:44 -0400

    updated proxy target

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000'
+        target: 'https://weather-app-new-nu-eight.vercel.app/'
       }
     }
   }
